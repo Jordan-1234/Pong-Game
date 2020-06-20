794be7844426300d89d9f81893a454351df2c2f9
Jordan-1234 <58566042+Jordan-1234@users.noreply.github.com>
2020-06-19 23:51:23 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -46,35 +46,43 @@ ball = pygame.Rect(ball_pos["x"], ball_pos["y"], 20, 20)
 player1 = pygame.Rect(0, 0, 20, 140)
 player2 = pygame.Rect(0, 0, 20, 140)
 
+# Creates centerized text in game window
 def draw_text(text_string, text_size, colour, x_pos, y_pos):
     font = pygame.font.Font("freesansbold.ttf", text_size)
     text = font.render(text_string, True, colour)
     text_rect = text.get_rect(center=(x_pos, y_pos))
     SCREEN.blit(text, text_rect)
 
+# Main Menu
 def starting_window():
     global ball_xspeed, ball_yspeed, scores, AI, main_menu, reset_vars
+    # Reseting variables after game is over
     if reset_vars:
         ball_xspeed = ball_speed * random.choice((-1, 1))
         ball_yspeed = ball_speed * random.choice((-1, 1))
         scores = {"player 1": 0, "player 2": 0}
         reset_vars = False
-
+    
+    # Select game mode through key press
     KEYS = pygame.key.get_pressed()
+    #If 1 is pressed during main menu. Start the game vs. AI
     if KEYS[pygame.K_1]:
         AI = True
         main_menu = False
+    #If 2 is pressed during main menu. Start the 2 player game
     elif KEYS[pygame.K_2]:
         main_menu = False
     draw_text("Pong", 100, WHITE, WIDTH//2, HEIGHT//2 - 100)
     draw_text("Press 1 for AI", 40, WHITE, WIDTH//2, HEIGHT//2 + 50)
     draw_text("Press 2 for 2 Players", 40, WHITE, WIDTH//2, HEIGHT//2 + 150)
 
+# Object movement
 def drawings_movement(AI):
     global player1_speed, player2_speed
+    # Ball Movement/Reposition
     ball_pos["x"] += ball_xspeed
     ball_pos["y"] += ball_yspeed
-
+    # Player 1 paddle movement (Press w to go up and press s to go down)
     KEYS = pygame.key.get_pressed()
     if KEYS[pygame.K_w] and KEYS[pygame.K_s]:
         player1_speed = 0
@@ -85,7 +93,9 @@ def drawings_movement(AI):
     else:
         player1_speed = 0
 
+    # Player 2 paddle movement
     if not AI:
+        # if two players, Press up arrow key to move up and down arrow key to move down)
         if KEYS[pygame.K_UP] and KEYS[pygame.K_DOWN]:
             player2_speed = 0
         elif KEYS[pygame.K_UP] and player2.top > 0:
@@ -94,6 +104,7 @@ def drawings_movement(AI):
             player2_speed = paddle_speed
         else:
             player2_speed = 0
+    # AI paddle code
     else:
         if ball_xspeed > 0:
             if ball.top < player2_pos["y"] and player2.top > 0:
@@ -102,6 +113,7 @@ def drawings_movement(AI):
                 player2_speed = paddle_speed
             else:
                 player2_speed = 0
+        # Moves AI paddle back to default location(center y position)
         else:
             if player2_pos["y"] < HEIGHT//2:
                 player2_speed = paddle_speed
@@ -109,13 +121,14 @@ def drawings_movement(AI):
                 player2_speed = -paddle_speed
             else:
                 player2_speed = 0
-
+    # Paddle vertical movement
     player1_pos["y"] += player1_speed
     player2_pos["y"] += player2_speed
 
-
+# Checks if ball collides with borders and paddles
 def ball_collision():
     global ball_xspeed, ball_yspeed
+    # Redirects ball if it hits player 1 paddle
     if ball.colliderect(player1) and ball_xspeed < 0:
         if abs(ball.left - player1.right) < abs(ball_xspeed):
             ball_xspeed *= -1
@@ -123,7 +136,7 @@ def ball_collision():
             ball_yspeed *= -1
         elif abs(ball.top - player1.bottom) < abs(ball_yspeed) and ball_yspeed < 0:
             ball_yspeed *= -1
-    
+    # Redirects ball if it hits player 2 paddle
     if ball.colliderect(player2) and ball_xspeed > 0:
         if abs(ball.right - player2.left) < abs(ball_xspeed):
             ball_xspeed *= -1
@@ -131,33 +144,35 @@ def ball_collision():
             ball_yspeed *= -1
         elif abs(ball.top - player2.bottom) < abs(ball_yspeed) and ball_yspeed < 0:
             ball_yspeed *= -1
-
+    # Redirects ball when it hits top or bottom border
     if abs(ball.top - 0) < abs(ball_yspeed) and ball_yspeed < 0:
         ball_yspeed *= -1
     elif abs(ball.bottom - HEIGHT) < abs(ball_yspeed) and ball_yspeed > 0:
         ball_yspeed *= -1
 
-
+# Displays drawings onto game window
 def display_drawings():
+    # Displays player scores
     draw_text(str(scores["player 1"]), 32, WHITE, WIDTH//2 - 100, 50)
     draw_text(str(scores["player 2"]), 32, WHITE, WIDTH//2 + 100, 50)
-       
+    
+    # Centerized ball and players position
     ball.center = (ball_pos["x"], ball_pos["y"])
     player1.center = (player1_pos["x"], player1_pos["y"])
     player2.center = (player2_pos["x"], player2_pos["y"])
-
+   
+    # Draws out the ball and paddles
     if not freeze_game:
         pygame.draw.ellipse(SCREEN, WHITE, ball)
     pygame.draw.rect(SCREEN, WHITE, player1)
     pygame.draw.rect(SCREEN, WHITE, player2)
 
-
+# When round start timer counts down
 def start_round():
     global ball_timer
     if ball_timer > 60:
         draw_text("3", 32, WHITE, WIDTH//2, HEIGHT//2 - 30)
      
-
     if 30 < ball_timer < 60:
         draw_text("2", 32, WHITE, WIDTH//2, HEIGHT//2 - 30)
 
@@ -166,7 +181,7 @@ def start_round():
     
     ball_timer -= 1
 
-
+# Ball and Player Position when round starts and resets.
 def reset_ball():
     global ball_timer
     if ball.x not in range(WIDTH):
@@ -176,6 +191,7 @@ def reset_ball():
         player1_pos["y"] = HEIGHT//2
         player2_pos["y"] = HEIGHT//2
         
+        # Increase player scores by 1 if ball pass and directs ball towards player who lost the last point
         if ball.right >= WIDTH:
             ball_xspeed = -ball_speed
             scores["player 1"] += 1
@@ -183,25 +199,26 @@ def reset_ball():
             ball_xspeed = ball_speed
             scores["player 2"] += 1
     
-
 def end_game():
     global main_menu, freeze_game, reset_vars
+    # Stops game components from moving (ball, paddle, countdown)
     freeze_game = True
     if scores["player 1"] == 10:
         draw_text("PLAYER 1 WINS",  60, WHITE, WIDTH//2, HEIGHT//2)
     elif scores["player 2"] == 10:
         draw_text("PLAYER 2 WINS",  60, WHITE, WIDTH//2, HEIGHT//2)
-    
+    # After game finishes, option to return to game menu to restart by pressing space
     draw_text("Press Space To Return To Main Menu", 30, WHITE, WIDTH//2, HEIGHT//2 + 100)
     KEYS = pygame.key.get_pressed()
     if KEYS[pygame.K_SPACE]:
         main_menu = True
         freeze_game = False
         reset_vars = True
-
+# Main game function
 def main():
     running = True
     while running:
+        # Option to close game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -209,11 +226,14 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
         
+        # Background black
         SCREEN.fill(BLACK)
-
+        
+        # Go to starting menu
         if main_menu:
             starting_window()
         else:
+            # Countdown runs if game not frozen 
             if ball_timer > 0 and not freeze_game:
                 start_round()
             else:
@@ -222,10 +242,11 @@ def main():
                     drawings_movement(AI)
             display_drawings()
             reset_ball()
+            # If player 1 or player 2 reach 10 points go to game menu
             if scores["player 1"] == 10 or scores["player 2"] == 10:
                 end_game()
             
-            
+        # Frame Rate and Update/Refresh Screen
         pygame.display.update()
         CLOCK.tick(60)
 
